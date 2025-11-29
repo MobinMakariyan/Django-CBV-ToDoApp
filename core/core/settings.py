0543fcc01dd7385d204575712d35c7b10c9cc4bb
MobinMakariyan <108710100+MobinMakariyan@users.noreply.github.com>
2025-11-29 19:47:24 +0330

    use drf-yasg for API documentation

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "accounts.apps.AccountsConfig",
+    "drf_yasg",
     "rest_framework",
+    "accounts.apps.AccountsConfig",
     "todo.apps.TodoConfig",
 ]
 
